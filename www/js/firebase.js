3d3dfda8fd73ba34054102fbe095cf0bde4f9630
DrAstral <91513012+DrAstral@users.noreply.github.com>
2021-11-01 21:52:50 -0300

    Add files via upload

---
+++ www/js/firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyDEP_BAIZpYtbgYehLjvp_NgEdhq6KgSxM",
+    authDomain: "testeaula2-453ae.firebaseapp.com",
+    projectId: "testeaula2-453ae",
+    storageBucket: "testeaula2-453ae.appspot.com",
+    messagingSenderId: "102331633007",
+    appId: "1:102331633007:web:48be1d5f565ffdf5706c9a",
+    measurementId: "G-0J84GM0Q7G"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics(app);
